411b39d6a1ef4ee2bdeb2a7137d50b75885bf5ef
zcl-scut <1918522414@qq.com>
2022-07-28 00:14:55 +0800

    主框架

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2 as cv
-from emotion import emotionFrameDetect as efd
+from emotion import emotionFrameDetect as emotion_detect
 
 class_labels={0:'Angry',1:'Fear',2:'Happy',3:'Neutral',4:'Sad',5:'Surprise'}
 classes=list(class_labels.values())
@@ -9,7 +9,7 @@ if __name__ =='__main__':
     camera=cv.VideoCapture(0)#打开摄像头
     while True:
         ret, frame = camera.read()
-        frame=efd(frame)
+        frame=emotion_detect(frame)#输入一帧原始图片，输出情绪识别后用文字标签后的图片
         cv.imshow('all', frame)
         if cv.waitKey(1) == 27 or cv.getWindowProperty("all",cv.WND_PROP_AUTOSIZE) != 1:  # ESC的ASCII码
             break
