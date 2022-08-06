af99d8257b56cc310bc0dbab4edfafdf7191ceaa
DThund <109129969+DThund@users.noreply.github.com>
2022-08-06 18:49:56 +0800

    Update Posture_project.py

--- PostureAnalysis/Posture_project.py
+++ PostureAnalysis/Posture_project.py
@@ -1,19 +1,20 @@
 import Tired as td
 import cv2
 
-def main():
-    # cv2.VideoCapture(0) cv2.VideoCapture("Trainer/1.mp4")
-    cap = cv2.VideoCapture("Trainer/1.mp4")
-    count = 0
-    dir = 0
-    pTime = 0
 
+def main():
+
+    # cv2.VideoCapture(0) cv2.VideoCapture("Trainer/1.mp4")
+    cap = cv2.VideoCapture(0)
+    pTime = 0
     while True:
 
         success, img = cap.read()
-        # img = cv2.imread("Trainer/test1.jpg")
-        count, dir, pTime = td.Posture_analysis(img, count, dir, pTime)
+        # img = cv2.imread("Trainer/right2.jpg")
+        img = cv2.resize(img, (1024, 768))
+        img, pTime = td.Posture_analysis(img, pTime)
 
 
 if __name__ == "__main__":
     main()
+
