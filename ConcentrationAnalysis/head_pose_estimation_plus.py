39a9cb13619454ace3b6d7808ef40e52a72988da
Fancilia <202030201130@mail.scut.edu.cn>
2022-08-16 16:46:36 +0800

    疲劳组集成

---
+++ ConcentrationAnalysis/head_pose_estimation_plus.py
@@ -0,0 +1,146 @@
+'''
+pitch最大值:18.63,最小值:-35.37
+yaw最大值:47.05,最小值:-49.59
+roll最大值:176.38,最小值:-179.37
+'''
+
+import time
+import cv2 as cv
+import numpy as np
+import dlib
+from imutils import face_utils # 人脸数据处理
+
+t0 = time.time()
+# 人脸检测模型
+detector = dlib.get_frontal_face_detector()
+# 人脸关键点预测
+predictor = dlib.shape_predictor('../lib/shape_predictor_68_face_landmarks.dat')
+t1 = time.time()
+print(f'模型加载时间: {round((t1 - t0) * 1000, 2)}ms')
+
+
+def get_image_points(shape):
+    landmarks = face_utils.shape_to_np(shape) # landmarks从0开始,与实际脸部标号差1
+    image_points = np.float32([landmarks[17], # 左眉左上角
+                               landmarks[21], # 左眉右上角
+                               landmarks[22], # 右眉左上角
+                               landmarks[26], # 右眉右上角
+                               landmarks[36], # 左眼左上角
+                               landmarks[39], # 左眼右上角
+                               landmarks[42], # 右眼左上角
+                               landmarks[45], # 右眼右上角
+                               landmarks[31], # 鼻子左上角
+                               landmarks[35], # 鼻子右上角
+                               landmarks[48], # 嘴左上角
+                               landmarks[54], # 嘴右上角
+                               landmarks[57], # 嘴中央下角
+                               landmarks[8]]) # 下巴角
+    return image_points
+
+
+# 根据2d点,3d点,相机内参,畸变参数,获得euler_angle
+def get_euler_angle(img_size, image_points):
+    # 3d模型坐标,参照https://github.com/by-sabbir/HeadPoseEstimation/blob/master/Visualize3DModel.py
+    object_points = np.float32([[6.825897, 6.760612, 4.402142],  # 左眉左上角
+                                [1.330353, 7.122144, 6.903745],  # 左眉右上角
+                                [-1.330353, 7.122144, 6.903745],  # 右眉左上角
+                                [-6.825897, 6.760612, 4.402142],  # 右眉右上角
+                                [5.311432, 5.485328, 3.987654],  # 左眼左上角
+                                [1.789930, 5.393625, 4.413414],  # 左眼右上角
+                                [-1.789930, 5.393625, 4.413414],  # 右眼左上角
+                                [-5.311432, 5.485328, 3.987654],  # 右眼右上角
+                                [2.005628, 1.409845, 6.165652],  # 鼻子左上角
+                                [-2.005628, 1.409845, 6.165652],  # 鼻子右上角
+                                [2.774015, -2.080775, 5.048531],  # 嘴左上角
+                                [-2.774015, -2.080775, 5.048531],  # 嘴右上角
+                                [0.000000, -3.116408, 6.097667],  # 嘴中央下角
+                                [0.000000, -7.415691, 4.070434]])  # 下巴角
+
+    # 焦距focal_length(相机坐标系与图像坐标系之间的距离为焦距f，也即图像坐标系原点与焦点重合)
+    focal_length = img_size[1]
+    center = (img_size[1] / 2, img_size[0] / 2)
+    camera_matrix = np.array(
+        [[focal_length, 0, center[0]],
+         [0, focal_length, center[1]],
+         [0, 0, 1]], dtype="double"
+    )
+
+    # 相机外参假设为0
+    dist_coeffs = np.zeros((4, 1))  # Assuming no lens distortion
+
+    _, rotation_vector, translation_vector = cv.solvePnP(objectPoints=object_points, imagePoints=image_points,
+                                                         cameraMatrix=camera_matrix, distCoeffs=dist_coeffs,
+                                                         flags=cv.SOLVEPNP_ITERATIVE)
+    rotation_mat, _ = cv.Rodrigues(rotation_vector)  # 将旋转矩阵向量转换为旋转矩阵
+    pose_mat = cv.hconcat((rotation_mat, translation_vector))  # 水平拼接
+    # eulerAngles –可选的三元素矢量，包含三个以度为单位的欧拉旋转角度
+    _, _, _, _, _, _, euler_angle = cv.decomposeProjectionMatrix(pose_mat)  # 计算euler_angle
+
+    return euler_angle
+
+
+# 将旋转矩阵转为欧拉角
+def trans_euler_angle(euler_angle):
+    pitch = euler_angle[0, 0]
+    yaw = euler_angle[1, 0]
+    roll = euler_angle[2, 0]
+
+    return pitch, yaw, roll
+
+
+def get_head_pose_estimate_frame(frame):
+    frame_size = frame.shape # 获取每帧的尺寸
+    # print(f'frame_size:{size},类型:{type(size)}')
+
+    gray=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    faces = detector(gray, 0)  # 检测人脸,运行消耗时间平均80ms
+    if faces: # 检测到人脸
+        for face in faces:
+            shape = predictor(frame, face)
+
+            image_points = get_image_points(shape)  # 获取面部2d关键点
+            euler_angle = get_euler_angle(img_size=frame_size, image_points=image_points)
+            pitch, yaw, roll = trans_euler_angle(euler_angle)
+            # print('pitch:{}, yaw:{}, roll:{}'.format(pitch, yaw, roll))
+
+            cv.putText(frame, f'pitch: {round(pitch, 2)}', (30, 150), fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                       fontScale=1, color=(0, 0, 255), thickness=2)
+            cv.putText(frame, f'yaw: {round(yaw, 2)}', (30, 180), fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                       fontScale=1, color=(0, 0, 255), thickness=2)
+            cv.putText(frame, f'roll: {round(roll, 2)}', (30, 210), fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                       fontScale=1, color=(0, 0, 255), thickness=2)
+
+            return round(pitch,2),round(yaw,2),round(roll,2),frame
+
+    else: # 未检测到人脸
+        pitch, yaw, roll = 0, 0, 0
+        return pitch,yaw,roll,frame
+
+
+
+
+def webcam_test():  # 摄像头获取帧数据测试
+    cap = cv.VideoCapture(0)
+    pitch_lst=[]
+    yaw_lst=[]
+    roll_lst=[]
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            pitch,yaw,roll,modified_frame = get_head_pose_estimate_frame(frame)
+            pitch_lst.append(pitch)
+            yaw_lst.append(yaw)
+            roll_lst.append(roll)
+            # 显示
+            cv.namedWindow('Frame', cv.WINDOW_NORMAL)
+            cv.imshow('Frame', modified_frame)
+            if cv.waitKey(1) == 27:
+                print(f'pitch最大值:{max(pitch_lst)},最小值:{min(pitch_lst)}')
+                print(f'yaw最大值:{max(yaw_lst)},最小值:{min(yaw_lst)}')
+                print(f'roll最大值:{max(roll_lst)},最小值:{min(roll_lst)}')
+                cv.destroyAllWindows()
+                break
+
+
+if __name__ == '__main__':
+    webcam_test()
--- src/main.py
+++ src/main.py
@@ -1,45 +1,63 @@
 import numpy as np
-import cv2 as cv
+import cv2
 import dlib
 import time
 from emotion import emotionFrameDetect as emotion_detect
 from posture import postureFrameDetect as posture_detect
 from concentration import concentrationFrameDetect as concentration_detect
 from concentration import get_overall_emotion_score,get_head_pose_score,get_fatigue_score,get_focus_grade
-from fatigue import fatigueFrameDetect as fatigue_detect
+from fatigue import eye_aspect_ratio,mouth_aspect_ratio,ear_mar,\
+    eye_params,mouth_params,get_fatigue,get_fatigue_grade,add_text
+# 表情组人脸识别模型加载
+face_detector = cv2.CascadeClassifier('../lib/haarcascade_frontalface_alt.xml ')
+
+# 疲劳度模型加载
+detector = dlib.get_frontal_face_detector() # 人脸识别
+predictor = dlib.shape_predictor('../lib/shape_predictor_68_face_landmarks.dat') # 关键点预测
 
 # 定义常数
-# 初始化帧计数器和眨眼总数
-eyeCounter = 0
-eyeTotal = 0
-# 初始化帧计数器和打哈欠总数
-mouthCounter = 0
-mouthTotal = 0
-# 初始化帧计数器和点头总数
-headCounter = 0
-headTotal = 0
-#情绪标签0~5
-emoFlag=0
 #emotion_detect函数输出数字标签，需查字典得到情绪类别
-emotion_dic={0:'Angry',1:'Fear',2:'Happy',3:'Neutral',4:'Sad',5:'Surprise'}
-#头部姿势标签
-headPosture=0
-posture_dic={0:'focus',1:'up',2:'down',3:'right',4:'left'}
-#是否疲劳
-isFatigue=0
-#是否专注
-isFocus=0
-#学习姿势是否正确
-isPosture=0
+emotion_dic={0:'Angry',1:'Fear',2:'Happy',3:'Neutral',4:'Sad',5:'Surprise',6:'Hate'}
+emotion_times_dict = {'Angry': 0, 'Hate': 0, 'Fear': 0, 'Happy': 0, 'Sad': 0, 'Surprise': 0, 'Neutral': 0, }
 
-face_detector = cv.CascadeClassifier('../lib/haarcascade_frontalface_alt.xml ')
+# 疲劳度的参数初始化
+period_frames = 1000  # 1000帧为一个周期
+step_frames = 10  # 10帧为一个检测步长
+frame_counter = 0  # 帧计数器
+fatigue = 0 # 初始化疲劳评分
+fatigue_grade = 1 # 疲劳等级初始化默认1级别
+# 初始化帧计数器和眨眼总数、闭眼总次数、perclose
+eye_conti_frames = 0  # 眼睛长宽比小于阈值的连续帧数
+blink_times = 0  # 周期内眨眼总次数
+eye_close_times = 0  # 闭眼总次数
+perclose = 0
+# 初始化帧计数器和打哈欠总数
+mouth_conti_frames = 0  # 嘴巴纵横比大于阈值的连续帧数
+yawn_times = 0  # 周期内打哈欠总数
+# 初始化闭眼时长、眨眼频率、和打哈欠频率
+eye_close_frames = 0  # 周期内连续闭眼总帧数
+blink_freq = 0  # 眨眼频率
+yawn_freq = 0  # 打哈欠频率
+
+# 专注度检测初始化
+# detect_times = 1000  # 设定一个周期处理帧数
+# counter = 0  # 帧计数(一周期清零一次)
+# pitch_lst = []  # 存放每帧数据的pitch
+# yaw_lst = []  # 存放每帧数据的yaw
+# yaw_times = 0  # 打哈欠次数
+# blink_times = 0  # 眨眼次数
+# eye_close_time_lst = []  # 一个周期中闭眼时长列表(从检测到闭眼开始计算时间,到检测到没有闭眼结束)
+# mouth_open_time_lst = []  # 一个周期中张嘴时长列表(从检测到打哈欠开始计算时间,到检测到没有打哈欠结束)
+# detect_time = 0  # 一个检测周期所消耗的总时长
+# focus_score = 0  # 初始专注度得分
+# focus_grade = None  # 初始专注度等级
 
 
 def faceDetectorVideo(img):
     # Convert image to grayscale
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #t = time.time()#测试执行时间
-    faces = face_detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10)
+    faces =face_detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10)
     #caleFactor=1.1, minNeighbors=10执行时间110ms
     #caleFactor=1.3, minNeighbors=10执行时间40ms
     #print(time.time() - t)#测试执行时间
@@ -48,87 +66,100 @@ def faceDetectorVideo(img):
         return (0, 0, 0, 0), np.zeros((48, 48), np.uint8), img
     # 检测到人脸，用(0,0,255)红色方框框出来
     for (x, y, w, h) in faces:
-        cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
         roi_gray = gray[y:y + h, x:x + w]#截取人脸，压缩后作为神经网络的输入，神经网络输出情绪标签
 
-    roi_gray = cv.resize(roi_gray, (48, 48), interpolation=cv.INTER_AREA)
+    roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
 
     return (x, w, y, h), roi_gray, gray
     # 返回人脸矩形参数，压缩人脸灰度图
 
 
 if __name__ =='__main__':
-    camera=cv.VideoCapture(0)#打开摄像头
-
-    # 专注度检测初始化
-    detect_times = 12 # 设定一个周期处理帧数
-    counter=0 # 帧计数(一周期清零一次)
-    emotion_times_dict={'Angry':0,'Hate':0,'Fear':0,'Happy':0,'Sad':0,'Surprise':0,'Neutral':0,}
-    pitch_lst=[] # 存放每帧数据的pitch
-    yaw_lst=[] # 存放每帧数据的yaw
-    yaw_times=0 # 打哈欠次数
-    blink_times=0 # 眨眼次数
-    eye_close_time_lst=[] # 一个周期中闭眼时长列表(从检测到闭眼开始计算时间,到检测到没有闭眼结束)
-    mouth_open_time_lst=[] # 一个周期中张嘴时长列表(从检测到打哈欠开始计算时间,到检测到没有打哈欠结束)
-    detect_time=0 # 一个检测周期所消耗的总时长
-    focus_score = 0 # 初始专注度得分
-    focus_grade = None # 初始专注度等级
-
+    camera=cv2.VideoCapture(0)#打开摄像头
     while True:
-
         ret, frame = camera.read()
         rect, roi_gray, gray = faceDetectorVideo(frame)# 输出人脸矩形坐标，压缩人脸灰度图
         frame_start_time = time.time() # 帧计时开始,测试模块执行时间
         emoFlag, photo = emotion_detect(rect,roi_gray,frame,frame)#输入灰度图，输出情绪类别标签emoFlag，并输出情绪识别后用文字标签后的图片frame
 
-        # 需要的参数：yaw_times,blink_times,eye_close_time_lst,mouth_open_time_lst
-        isFatigue, photo = fatigue_detect(frame, photo)
-        isPosture, photo = posture_detect(frame, photo)
+        # --------疲劳度模块------------
+        rects = detector(gray, 0) # 这个地方重复,但换成前面的rects会出错
+        # 第七步：循环脸部位置信息，使用predictor(gray, rect)获得脸部特征位置的信息
+        for rect in rects:
+            # 计算ear,mar
+            ear, mar = ear_mar(gray, rect)
+
+            # 第十三步：循环，满足条件的，眨眼次数+1
+            ear, eye_conti_frames, step_frames, blink_times, eye_close_times, eye_close_frames = eye_params(ear, \
+                eye_conti_frames,step_frames,blink_times, eye_close_times,eye_close_frames)
+
+            # 眨眼频率
+            blink_freq = blink_times / period_frames
+            # perclose
+            perclose = eye_close_frames / period_frames
+            # 同理，判断是否打哈欠
+            mar, mouth_conti_frames, step_frames, yawn_times = mouth_params(mar, mouth_conti_frames, step_frames,
+                                                                            yawn_times)
+            # 打哈欠频率
+            yawn_freq = yawn_times / period_frames
+            # 疲劳得分
+            fatigue = get_fatigue(blink_freq, yawn_freq, perclose, eye_close_times)
+            # 疲劳等级
+            fatigue_grade = get_fatigue_grade(fatigue)
+            # 添加文字
+            add_text(frame, eye_conti_frames, ear, blink_times, blink_freq, mouth_conti_frames, mar, yawn_times,
+                     yawn_freq, eye_close_times, perclose, rects, frame_counter, fatigue, fatigue_grade)
+
+            if frame_counter == period_frames:  # 一个计数周期结束
+                # cv2.imwrite('./out.png', frame)
+
+                # 进入下一个周期,参数初始化
+                period_frames = 100  # 1000帧为一个周期
+                step_frames = 10  # 10帧为一个检测步长
+                frame_counter = 0  # 帧计数器
+                # 初始化疲劳评分
+                fatigue = 0
+                fatigue_grade = 'None'
+                # 初始化帧计数器和眨眼总数、闭眼总次数、perclose
+                eye_conti_frames = 0  # 眼睛长宽比小于阈值的连续帧数
+                blink_times = 0  # 周期内眨眼总次数
+                eye_close_times = 0  # 闭眼总次数
+                perclose = 0
+                # 初始化帧计数器和打哈欠总数
+                mouth_conti_frames = 0  # 嘴巴纵横比大于阈值的连续帧数
+                yawn_times = 0  # 周期内打哈欠总数
+                # 初始化闭眼时长、眨眼频率、和打哈欠频率
+                eye_close_frames = 0  # 周期内连续闭眼总帧数
+                blink_freq = 0  # 眨眼频率
+                yawn_freq = 0  # 打哈欠频率
+
+
+
 
         # --------专注度模块------------
-        rect_dlib=[]
-        (x,w,y,h)=rect
-        rect_dlib.append(dlib.rectangle(x,y,x+w,y+h))
-        pitch,yaw,roll, photo = concentration_detect(rect_dlib,gray, photo)
-
-        counter+=1 # 计数+1帧
-        emotion_times_dict[emotion_dic[emoFlag]]=emotion_times_dict[emotion_dic[emoFlag]]+1
-        pitch_lst.append(pitch)
-        yaw_lst.append(yaw)
-        detect_time += (time.time() -frame_start_time)
-
-        if counter==detect_times: # 一个周期检测结束
-            # 处理数据
-            head_pose_score=get_head_pose_score(pitch_lst,yaw_lst)
-            overall_emotion_score=get_overall_emotion_score(detect_times,emotion_times_dict)
-            fatigue_score=get_fatigue_score(detect_times,eye_close_time_lst,yaw_times,
-                                            mouth_open_time_lst,blink_times,detect_time)
-            focus_score = 0.07192743 * overall_emotion_score + 0.27895457 * fatigue_score \
-                          + 0.649118 * head_pose_score
-            focus_grade=get_focus_grade(focus_score)
-            print(f'专注度得分为:{round(focus_score,2)}')
-            print(f'检测了{counter}帧,消耗了{round(detect_time,2)}s,平均每帧消耗{round(1000*detect_time/counter,2)}ms')
-
-            # 变量清零
-            counter = 0  # 帧计数(一周期清零一次)
-            emotion_times_dict = {'Angry': 0, 'Hate': 0, 'Fear': 0, 'Happy': 0, 'Sad': 0, 'Surprise': 0, 'Neutral': 0, }
-            pitch_lst = []  # 存放每帧数据的pitch
-            yaw_lst = []  # 存放每帧数据的yaw
-            yaw_times = 0  # 打哈欠次数
-            blink_times = 0  # 眨眼次数
-            eye_close_time_lst = []  # 一个周期中闭眼时长列表(从检测到闭眼开始计算时间,到检测到没有闭眼结束)
-            mouth_open_time_lst = []  # 一个周期中张嘴时长列表(从检测到打哈欠开始计算时间,到检测到没有打哈欠结束)
-            detect_time = 0  # 一个检测周期所消耗的总时长
-
-
-        cv.putText(photo, f'focus_score:{round(focus_score, 2)}', (20, 190), fontFace=cv.FONT_HERSHEY_SIMPLEX,
-                   fontScale=0.8, color=(0, 0, 255), thickness=2)
-        cv.putText(photo, f'focus_grade:{str(focus_grade)}', (20, 220), fontFace=cv.FONT_HERSHEY_SIMPLEX,
-                   fontScale=0.8, color=(0, 0, 255), thickness=2)
-        #print(camera.get(cv.CAP_PROP_FPS))
+        # rect_dlib=[]
+        # (x,w,y,h)=rect
+        # rect_dlib.append(dlib.rectangle(x,y,x+w,y+h))
+        # pitch,yaw,roll, photo = concentration_detect(rect_dlib,gray, photo)
+        #
+        # counter+=1 # 计数+1帧
+        # emotion_times_dict[emotion_dic[emoFlag]]=emotion_times_dict[emotion_dic[emoFlag]]+1
+        # pitch_lst.append(pitch)
+        # yaw_lst.append(yaw)
+        # detect_time += (time.time() -frame_start_time)
+        #
+        #
+        #
+        #
+        # cv2.putText(photo, f'focus_score:{round(focus_score, 2)}', (20, 190), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        #            fontScale=0.8, color=(0, 0, 255), thickness=2)
+        # cv2.putText(photo, f'focus_grade:{str(focus_grade)}', (20, 220), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        #            fontScale=0.8, color=(0, 0, 255), thickness=2)
+        print(camera.get(cv2.CAP_PROP_FPS))
         print('处理一帧所需时间:{:.5f}ms'.format(1000*(time.time() -frame_start_time)))#测试模块执行时间
-        cv.imshow('all', photo)
-        if cv.waitKey(1) == 27 or cv.getWindowProperty("all",cv.WND_PROP_AUTOSIZE) != 1:  # ESC的ASCII码
+        cv2.imshow('all', photo)
+        if cv2.waitKey(1) == 27 or cv2.getWindowProperty("all",cv2.WND_PROP_AUTOSIZE) != 1:  # ESC的ASCII码
             break
     camera.release()
-    cv.destroyAllWindows()
+    cv2.destroyAllWindows()
